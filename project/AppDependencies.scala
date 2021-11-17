322788e08ec02c8562107a3a70cf2cb763b80d35
aerodigi <54714520+aerodigi@users.noreply.github.com>
2021-11-17 11:56:17 +0000

    [HMA-4845] Play 2.8 Upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,21 +4,22 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapPlayVersion     = "5.1.0"
-  private val playHmrcVersion          = "6.2.0-play-27"
+  private val bootstrapPlayVersion     = "5.16.0"
+  private val playHmrcVersion          = "6.4.0-play-28"
   private val scalatestPlusPlayVersion = "4.0.3"
 
   private val scalaMockVersion       = "4.1.0"
   private val pegdownVersion         = "1.6.0"
-  private val integrationTestVersion = "1.1.0-play-27"
+  private val integrationTestVersion = "1.2.0-play-28"
   private val cucumberJunitVersion   = "1.2.6"
   private val cucumberScalaVersion   = "1.2.6"
   private val wiremockVersion        = "2.27.2"
   private val refinedVersion         = "0.9.19"
+  private val flexmarkAllVersion     = "0.35.10"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-backend-play-27" % bootstrapPlayVersion,
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapPlayVersion,
     "uk.gov.hmrc" %% "play-hmrc-api"             % playHmrcVersion,
     "eu.timepit"  %% "refined"                   % refinedVersion
   )
@@ -36,7 +37,8 @@ object AppDependencies {
         override lazy val test: Seq[ModuleID] = Seq(
           "org.scalatestplus.play" %% "scalatestplus-play" % scalatestPlusPlayVersion % scope,
           "org.scalamock"          %% "scalamock"          % scalaMockVersion         % scope,
-          "org.pegdown"            % "pegdown"             % pegdownVersion           % scope
+          "org.pegdown"            % "pegdown"             % pegdownVersion           % scope,
+          "com.vladsch.flexmark"   % "flexmark-all"        % flexmarkAllVersion       % scope
         )
       }.test
   }
@@ -55,7 +57,8 @@ object AppDependencies {
           "com.typesafe.play"      %% "play-test"                % PlayVersion.current      % scope,
           "info.cukes"             %% "cucumber-scala"           % cucumberJunitVersion     % scope,
           "info.cukes"             % "cucumber-junit"            % cucumberScalaVersion     % scope,
-          "com.github.tomakehurst" % "wiremock"                  % wiremockVersion          % scope
+          "com.github.tomakehurst" % "wiremock"                  % wiremockVersion          % scope,
+          "com.vladsch.flexmark"   % "flexmark-all"              % flexmarkAllVersion       % scope
         )
       }.test
 
