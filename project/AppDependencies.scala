51d64a4309c0c0911a77bd896d28c843efc94985
Martin Rolfe <52818407+mrolfe88@users.noreply.github.com>
2024-03-15 12:15:12 +0000

    [HMA-7994] Update play minor version (#31)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,19 +1,15 @@
-import play.core.PlayVersion
 import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
 
-  private val bootstrapPlayVersion     = "5.24.0"
-  private val playHmrcVersion          = "7.0.0-play-28"
-  private val scalatestPlusPlayVersion = "4.0.3"
+  private val bootstrapPlayVersion = "7.19.0"
+  private val playHmrcVersion      = "7.2.0-play-28"
 
-  private val scalaMockVersion       = "4.1.0"
-  private val pegdownVersion         = "1.6.0"
-  private val integrationTestVersion = "1.2.0-play-28"
-  private val wiremockVersion        = "2.27.2"
-  private val refinedVersion         = "0.9.19"
-  private val flexmarkAllVersion     = "0.35.10"
+  private val scalaMockVersion = "5.1.0"
+  private val pegdownVersion   = "1.6.0"
+  private val wiremockVersion  = "2.27.2"
+  private val refinedVersion   = "0.9.26"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
@@ -28,10 +24,8 @@ object AppDependencies {
   }
 
   private def testCommon(scope: String) = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapPlayVersion     % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play"     % scalatestPlusPlayVersion % scope,
-    "org.pegdown"            % "pegdown"                 % pegdownVersion           % scope,
-    "com.vladsch.flexmark"   % "flexmark-all"            % flexmarkAllVersion       % scope
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapPlayVersion % scope,
+    "org.pegdown" % "pegdown"                 % pegdownVersion       % scope
   )
 
   object Test {
@@ -53,8 +47,7 @@ object AppDependencies {
         override lazy val scope: String = "it"
 
         override lazy val test: Seq[ModuleID] = testCommon(scope) ++ Seq(
-            "com.typesafe.play"      %% "play-test" % PlayVersion.current % scope,
-            "com.github.tomakehurst" % "wiremock"   % wiremockVersion     % scope
+            "com.github.tomakehurst" % "wiremock" % wiremockVersion % scope
           )
       }.test
 
